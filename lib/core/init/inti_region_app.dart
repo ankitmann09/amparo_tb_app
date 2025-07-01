0bf2bde72a1646c9d9e8484a706d17ceea24a2cc
ankitmann09 <ankit.mann@yorbl.com>
2025-07-01 12:16:43 +0530

    Fix: Wait for initialization before showing login page to avoid tbClient error

--- lib/core/init/inti_region_app.dart
+++ lib/core/init/inti_region_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:thingsboard_app/core/auth/login/select_region_screen.dart';
+import 'package:thingsboard_app/core/auth/login/login_page.dart';
 import 'package:thingsboard_app/core/context/tb_context_widget.dart';
 import 'package:thingsboard_app/locator.dart';
 import 'package:thingsboard_app/utils/services/local_database/i_local_database_service.dart';
@@ -16,7 +16,7 @@ class _ThingsboardInitAppState extends TbPageState<ThingsboardInitRegionApp> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getIt<ILocalDatabaseService>().getSelectedRegion(),
+      future: initTbContext(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Scaffold(
@@ -27,20 +27,12 @@ class _ThingsboardInitAppState extends TbPageState<ThingsboardInitRegionApp> {
             ),
           );
         }
-
-        if (snapshot.hasError || snapshot.data == null) {
-          return SelectRegionScreen(tbContext);
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: Text('Initialization error: \\${snapshot.error}')),
+          );
         }
-
-        initTbContext();
-
-        return Scaffold(
-          body: Container(
-            alignment: Alignment.center,
-            color: Colors.white,
-            child: TbProgressIndicator(tbContext, size: 50.0),
-          ),
-        );
+        return LoginPage(tbContext);
       },
     );
   }
